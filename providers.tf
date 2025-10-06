2a882c2d1240436840dbfdccd07ef8e79e261cf3
sneha-pasam <98451233+sneha-pasam@users.noreply.github.com>
2025-10-06 17:12:22 +0530

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,13 @@
+# Define the AWS Provider and Region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
