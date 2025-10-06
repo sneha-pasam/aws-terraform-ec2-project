093e9d8324038739e0ccd6231f8e4612b86f075e
sneha-pasam <98451233+sneha-pasam@users.noreply.github.com>
2025-10-06 16:42:32 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,9 @@
+output "web_server_public_ip" {
+  description = "The public IP address of the deployed web server."
+  value       = aws_instance.web_server.public_ip
+}
 
+output "security_group_id" {
+  description = "The ID of the Security Group created for the web server."
+  value       = aws_security_group.web_sg.id
+}
