8864409b21c92be9690119efcdbde012b3c9b6c5
sneha-pasam <98451233+sneha-pasam@users.noreply.github.com>
2025-10-06 16:41:53 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,13 @@
+# The size/type of the EC2 instance
+variable "instance_type" {
+  description = "The size of the EC2 instance."
+  type        = string
+  default     = "t2.micro" # Free-tier eligible instance type
+}
 
+# The AWS region where resources will be provisioned
+variable "aws_region" {
+  description = "The AWS region to deploy resources into."
+  type        = string
+  default     = "ap-south-1" # Based on your Project 1 location
+}
